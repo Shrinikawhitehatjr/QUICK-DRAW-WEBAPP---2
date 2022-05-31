5da2d88fcf3f112b8a26ef394ef153cbbe2b8452
Shrinikawhitehatjr <97172285+Shrinikawhitehatjr@users.noreply.github.com>
2022-05-31 19:52:28 +0530

    Add files via upload

---
+++ main (1).js
@@ -0,0 +1,40 @@
+function draw() {
+    check_sketch() 
+            if(stroke=='black'|| stroke=='red'){
+                line(30,40,25,50)
+                answer_holder {"set"};
+                <p id="label">Score: </p>
+                }
+            }
+
+function check_sketch() 
+    if(condition){
+        counter++
+        span = time "Timer"();
+        var timer_counter
+        set timer_counter() = 0;
+        timer_check value = 400;
+        if (condition) {
+            
+        timer_value = completed;
+        var timer_check ("");
+        var answer_holder ("");
+            
+        }
+}
+
+function updateCanvas() {
+
+    Math.floor(1, 5, 8, 12, 16);
+    console.log(quick_draw_data_set);
+    background("white");
+}
+
+function setUp() {
+    canvas = createCanvas(280, 280);
+    canvas.center();
+
+}
+
+function updateCanvas(){
+}
